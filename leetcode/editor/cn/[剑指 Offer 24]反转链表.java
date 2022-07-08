62f27bd2ceaa53221668c84005b969be9219d272
chengshuaihu <chengshuaihu@meituan.com>
2022-07-08 18:28:53 +0800

    offer24递归

--- leetcode/editor/cn/[剑指 Offer 24]反转链表.java
+++ leetcode/editor/cn/[剑指 Offer 24]反转链表.java
@@ -20,28 +20,40 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
  * }
  */
 class Solution {
+//    public ListNode reverseList(ListNode head) {
+//        if (head == null){
+//            return head;
+//        }
+//        ListNode prev = null;
+//        ListNode next = null;
+//        while (head != null){
+//            next = head.next;
+//            head.next= prev;
+//            prev = head;
+//            head = next;
+//        }
+//        return prev;
+//    }
+
     public ListNode reverseList(ListNode head) {
-        if (head == null){
+        if (head == null || head.next == null) {
             return head;
         }
-        ListNode prev = null;
-        ListNode next = null;
-        while (head != null){
-            next = head.next;
-            head.next= prev;
-            prev = head;
-            head = next;
-        }
-        return prev;
+
+        ListNode node = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return node;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
