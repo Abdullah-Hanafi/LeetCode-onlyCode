0d883692ae4831361a70c7a109683d621d4be4ba
chengshuaihu <chengshuaihu@meituan.com>
2022-07-08 17:59:30 +0800

    offer25递归

--- leetcode/editor/cn/[剑指 Offer 25]合并两个排序的链表.java
+++ leetcode/editor/cn/[剑指 Offer 25]合并两个排序的链表.java
@@ -34,21 +34,13 @@ class Solution {
             return l1;
         }
 
-        ListNode dummyNode = new ListNode(-1);
-        ListNode cur = dummyNode;
-
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                cur.next = l1;
-                l1 = l1.next;
-            } else {
-                cur.next = l2;
-                l2 = l2.next;
-            }
-            cur = cur.next;
+        if (l1.val <= l2.val){
+            l1.next = mergeTwoLists(l1.next,l2);
+            return l1;
+        }else {
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
         }
-        cur.next = (l1 != null) ? l1 : l2;
-        return dummyNode.next;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
