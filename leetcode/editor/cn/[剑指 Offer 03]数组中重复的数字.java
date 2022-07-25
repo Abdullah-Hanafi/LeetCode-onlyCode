e0d7c1480134280936e2ba147ba7cda95141c60f
chengshuaihu <chengshuaihu@meituan.com>
2022-07-25 20:53:57 +0800

    Offer3

--- leetcode/editor/cn/[剑指 Offer 03]数组中重复的数字.java
+++ leetcode/editor/cn/[剑指 Offer 03]数组中重复的数字.java
@@ -24,13 +24,13 @@ class Solution {
     public int findRepeatNumber(int[] nums) {
         //利用
         Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++){
-            if (set.contains(nums[i])){
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
                 return nums[i];
             }
             set.add(nums[i]);
         }
-        return nums[0];
+        return -1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
