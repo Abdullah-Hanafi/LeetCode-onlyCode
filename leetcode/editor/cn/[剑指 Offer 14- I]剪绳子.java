895b322901f9135ec0477e15a842599bbe26d9f4
shuaichengpku <15638286516@163.com>
2022-08-25 15:15:08 +0800

    Offer14-I

--- leetcode/editor/cn/[剑指 Offer 14- I]剪绳子.java
+++ leetcode/editor/cn/[剑指 Offer 14- I]剪绳子.java
@@ -31,7 +31,7 @@ class Solution {
         int a = n / 3;
         int b = n % 3;
         if (b == 0) return (int) Math.pow(3, a);
-        if (b == 1) return (int) Math.pow(3, a - 1) * 4;
+        if (b == 1) return (int) Math.pow(3, a - 1) * 2 * 2;
         return (int) Math.pow(3, a) * 2;
     }
 }
