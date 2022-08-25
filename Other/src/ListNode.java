53f29c479e337ec8a5396cc0dec51a6a700ec26f
shuaichengpku <15638286516@163.com>
2022-08-25 10:31:40 +0800

    电脑接入github

--- Other/src/ListNode.java
+++ Other/src/ListNode.java
@@ -3,14 +3,11 @@ import java.util.List;
 public class ListNode {
     int val;
     ListNode next;
-
     public ListNode() {
     }
-
     public ListNode(int val) {
         this.val = val;
     }
-
     public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
